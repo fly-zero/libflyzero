94a4bb22ce2cd32c8f1b6942d025338ebd5dd794
flyzero <fly.zero@foxmail.com>
2017-12-14 16:23:10 +0800

    new file:   file_descriptor.h 	deleted:    FileDescriptor.h 	modified:   TcpConnection.h 	modified:   TcpServer.cpp 	modified:   TcpServer.h 	modified:   epoll.h

---
+++ file_descriptor.h
@@ -0,0 +1,95 @@
+#pragma once
+
+#include <unistd.h>
+#include <fcntl.h>
+
+namespace flyzero
+{
+
+    class file_descriptor
+    {
+    public:
+        constexpr file_descriptor() = default;
+
+        explicit file_descriptor(int const fd) noexcept
+            : fd_(fd)
+        {
+        }
+
+        file_descriptor(const file_descriptor & other) noexcept
+            : fd_(other.fd_ == -1 ? -1 : ::dup(other.fd_))
+        {
+        }
+
+        file_descriptor(file_descriptor && other) noexcept
+            : fd_(other.fd_)
+        {
+            other.fd_ = -1;
+        }
+
+        ~file_descriptor() noexcept
+        {
+            if (fd_ >= 0) ::close(fd_);
+        }
+
+        int get() const noexcept
+        {
+            return fd_;
+        }
+
+        void close() noexcept
+        {
+            ::close(fd_);
+            fd_ = -1;
+        }
+
+        file_descriptor clone() const noexcept
+        {
+            return file_descriptor(::dup(fd_));
+        }
+
+        bool set_nonblocking() const noexcept
+        {
+            auto const ret = fcntl(fd_, F_GETFL);
+            if (ret == -1)
+                return false;
+            return fcntl(fd_, F_SETFL, ret | O_NONBLOCK) != -1;
+        }
+
+        bool operator!() const noexcept
+        {
+            return fd_ == -1;
+        }
+
+        explicit operator bool() const noexcept
+        {
+            return fd_ != -1;
+        }
+
+        file_descriptor & operator=(const file_descriptor & other) noexcept
+        {
+            if (this != &other)
+                fd_ = other.fd_ == -1 ? -1 : ::dup(other.fd_);
+            return *this;
+        }
+
+        file_descriptor & operator=(file_descriptor && other) noexcept
+        {
+            if (this != &other)
+            {
+                fd_ = other.fd_;
+                other.fd_ = -1;
+            }
+            return *this;
+        }
+
+        bool operator<(const file_descriptor & other) const noexcept
+        {
+            return fd_ < other.fd_;
+        }
+
+    private:
+        int fd_{ -1 };
+    };
+
+}
