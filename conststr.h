2bfe47a70ae7c288c51bf17d043209d7184b3eeb
flyzero <fly.zero@foxmail.com>
2017-12-15 10:57:37 +0800

    deleted:    Chunked.cpp 	deleted:    Chunked.h 	modified:   conststr.h

--- conststr.h
+++ conststr.h
@@ -5,21 +5,26 @@ namespace flyzero
 {
     class conststr
     {
-        const char* str_;
-        std::size_t length_;
-
     public:
         template<std::size_t N>
         explicit constexpr conststr(const char(&a)[N]) : str_(a), length_(N - 1) { }
 
         // constexpr functions signal errors by throwing exceptions
         // in C++11, they must do so from the conditional operator ?:
-        constexpr char operator[](std::size_t n) const
+        constexpr char operator[](const std::size_t n) const
         {
             return n < length_ ? str_[n] : throw std::out_of_range("");
         }
 
         constexpr std::size_t size() const { return length_; }
+
+        const char * begin() const { return str_; }
+
+        const char * end() const { return str_ + length_; }
+
+    private:
+        const char* str_;
+        std::size_t length_;
     };
 }
 
